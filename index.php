ca9bad0c28d4cd164fa4aa6e98039c98e3b89ba3
63070248 <73692033+63070248@users.noreply.github.com>
2020-10-30 17:43:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+    <head>
+        <title> Hello cat world!!!</title>
+    </head>
+    <body>
+        <h1>Hello cat world!!!</h1>
+        <p>สุภัสสรา วงษ์ศรี ชื่อเล่น ลูกน้ำ</p>
+        <img src="cat.jpeg">
+        <br>
+        <img src="catt.jpg">
+        <br>
+        <a href="https://www.google.com">This is a Link</a>
+        
+        <h2>talk talk talk!!!</h2>
+        <ul>
+            <li>เราปล่อยแมวออกมาวิ่งเล่นในสวน  แล้วเค้าวิ่งขึ้นต้นไม้ไปชะเง้อดูนก
+            <br>เราก็นั่งอ่านหนังสือรออยู่ที่โต๊ะไม้ยาวใต้ต้นไม้นั่นแหละค่ะ=
+            <br>รอน๊านนานจนลืมไปเลยว่าลูกอยู่บนต้นไม้</li>
+            
+        </ul>
+    </body>
+</html>
